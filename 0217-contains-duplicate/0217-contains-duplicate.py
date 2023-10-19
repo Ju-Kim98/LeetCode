ec2823cf82a7fc19274b2a0e92a140fc0caf6766
Ju-Kim98 <130087422+Ju-Kim98@users.noreply.github.com>
2023-10-18 19:02:10 -0700

    Time: 435 ms (36.46%), Space: 29 MB (44.87%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+       
+        HashSet = set()  #set: 집합, 중복x, 순서x / O(1) time
+        for n in nums:      #O(n)
+            if n in HashSet:
+                return True
+            HashSet.add(n) 
+        else:
+            return False
+        
+    
+        
+        """
+        
+        #sorting을 해서 그 리스트를 for룹 옆에있는 숫자 비교
+        #sort func O(nlogn)
+        nums.sort(nums): 
+        for n in range(nums):
+            if n == n+1:
+                return True
+                n++
+            else:
+                return False 
+                """
+            
