e1bc49efc97b31163f4012f626d7751cbef2119d
Ju-Kim98 <130087422+Ju-Kim98@users.noreply.github.com>
2023-11-01 18:59:38 -0700

    Time: 72 ms (56.16%), Space: 17.2 MB (80.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -11,13 +11,25 @@ class Solution(object):
         #사용한 문자(key), 해당 문자의 사용횟수(value)
         
         #strs = {"ate","eat","tea","bat"}
+  
         
         result = collections.defaultdict(list)  # default hashmap:[eat,tea,ate] O(mn26)
 
+        #23ms      
+        for st in strs:
+             s = ''.join(sorted(st))
+             result[s].append(st)
+
+    	return result.values()
+        
+        
+        
+        """
+        #17ms
         for s in strs:
             count = [0]*26  #count the char a - z, 각 단어에 있는 알파벳 개수 e:1, a:1, t:1  <- 이게 key가 됨,e,a,t가 하나씩 있는 단어들을 분별해서 hashlist에 집어넣기
             for c in s: 
                 count[ord(c)-ord("a")] += 1  #current aski value from c, a(80), b(81)    b-a = 81-80=1
             result[tuple(count)].append(s)  #count is the list, in python list can't be key so use tuple.
         return result.values()
-    
+    """
