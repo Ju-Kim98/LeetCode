feeada0d028299001e06aa27913f238da5f57f24
Ju-Kim98 <130087422+Ju-Kim98@users.noreply.github.com>
2023-11-15 18:37:23 -0800

    Time: 284 ms (17.02%), Space: 20.6 MB (82.90%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.py
@@ -4,9 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        # do it w/o extra space and in O(n) runtime
+        # do it w/o extra space and in O(n) runtime => Time: O(n) Space: O(1f7)
         for n in nums:
-            i = abs(n) - 1
+            i = abs(n)- 1
             nums[i] = -1 * abs(nums[i])
         
         #collect the missing values which are still positive
@@ -19,5 +19,9 @@ class Solution(object):
     
 #abs() : return absolute val (절댓값)
 
-#34ms sol)
-       #return set(range(1, len(nums)+1)) - set(nums)
+#34ms
+       #return set(range(1, len(nums)+1)) - set(nums) 
+    #set()의 time complexity: O(1) or O(n)
+        
+    
+
