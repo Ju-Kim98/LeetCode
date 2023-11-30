56cd01a583187b8b662b30d9acf4e53ba894f362
Ju-Kim98 <130087422+Ju-Kim98@users.noreply.github.com>
2023-11-29 18:56:56 -0800

    Time: 16 ms (92.13%), Space: 13.4 MB (62.50%) - LeetHub

--- 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
@@ -5,13 +5,39 @@ class Solution(object):
         :type c: str
         :rtype: List[int]
         """
+    # O(n^2) Time sol
+    
         n = len(s)
         answer = [0] * n
-        c_positions = [i for i in range(n) if s[i] == c]
+        
+        #ind = [i for i in range(n) if s[i] == c]  #index position of c
 
-        # Iterate through each character in the string
+        #for i in range(n):
+        #    answer[i] = min([abs(i - j) for j in ind])  
+
+        #return answer
+
+    # O(n) Time sol
+      
+        left = [float('inf')] *n
+        right =[float('inf')] *n
+           
+        temp = float('inf')
         for i in range(n):
-            # Compute the minimum distance to any occurrence of c
-            answer[i] = min([abs(i - pos) for pos in c_positions])
+            if s[i] == c:
+                temp = 0
+            left[i] = temp
+            temp += 1
+
+        #temp = float('inf')
+        for i in reversed(range(n)):
+            if s[i] == c:
+                temp = 0
+            right[i] = temp
+            temp += 1
+
+        for i in range(n):
+            answer[i] = min(left[i], right[i])
 
         return answer
+    
