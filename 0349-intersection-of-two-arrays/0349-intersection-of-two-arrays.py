454f91d0c37b26d990c9006fd9cc8304bd2244dd
Ju-Kim98 <130087422+Ju-Kim98@users.noreply.github.com>
2024-01-25 16:42:16 -0800

    Time: 28 ms (65.61%), Space: 11.7 MB (98.61%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -6,25 +6,40 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        #sort O(nlogn)
-        nums1.sort()
-        nums2.sort()
+#         #sort O(nlogn)
+#         nums1.sort()
+#         nums2.sort()
         
-        #initializing pointers
-        i=0;
-        j=0;
+#         #initializing pointers
+#         i=0;
+#         j=0;
         
-        inter_set = set()
+#         inter_set = set()
         
-        while i < len(nums1) and j < len(nums2):
-            if nums1[i] == nums2[j]:
-                #add to set if same ele
-                inter_set.add(nums1[i])
-                i+=1
-                j+=1
-            elif nums1[i]<nums2[j]:
-                i+=1
-            else: 
-                j+=1
+#         while i < len(nums1) and j < len(nums2):
+#             if nums1[i] == nums2[j]:
+#                 #add to set if same ele
+#                 inter_set.add(nums1[i])
+#                 i+=1
+#                 j+=1
+#             elif nums1[i]<nums2[j]:
+#                 i+=1
+#             else: 
+#                 j+=1
                 
-        return list(inter_set)
+#         return list(inter_set)
+    
+    
+    #sort를 안 쓰는 방법
+    
+    # Use a set for constant time look-up
+        nums1_set = set(nums1)
+        result_set = set()
+
+        # Check each element in nums2
+        for num in nums2:
+            if num in nums1_set:
+                result_set.add(num)
+
+        # Convert the result set to a list
+        return list(result_set)
