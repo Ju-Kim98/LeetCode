94e756a66aab064f9a1fad16d781ce8b36f859a4
Ju-Kim98 <130087422+Ju-Kim98@users.noreply.github.com>
2024-01-25 17:43:14 -0800

    Time: 172 ms (93.87%), Space: 13.1 MB (91.72%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,17 +1,25 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        #using two boundary indexes, left and rightt
-        left = 0
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        # O(logn) time
+        
+        left = 0;
         right = len(nums)-1
         
         while left<=right:
-            mid=(left+right)//2
+            mid = (left+right)//2
             
-            if nums[mid]==target:
+            if nums[mid] == target:
                 return mid
-            elif nums[mid]<= target:
-                left=mid+1
+            elif nums[mid]<target:
+                left = mid+1
             else:
-                right=mid-1
-            
+                right = mid-1
+                
         return -1
+        
