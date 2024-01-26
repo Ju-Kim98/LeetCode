9f0d66da3d254465eb6d148e072469798ac881b0
Ju-Kim98 <130087422+Ju-Kim98@users.noreply.github.com>
2024-01-25 16:26:58 -0800

    Time: 12 ms (80.88%), Space: 11.8 MB (96.55%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -8,15 +8,21 @@ class Solution(object):
         #stack to track opening brackets
         stack = []
 
-        #hash mapping
-        h_map = {"]":"[", "}":"{", ")":"("}
+        #hash mapping - for quick lookups to match pairs
+        h_map = {"]":"[", "}":"{", ")":"("}  # values:keys
         
         for char in s:
+            # if cur char is opening, append to stack
             if char in h_map.values():
                 stack.append(char)
+                
+            # if cue char is closing,    
             elif char in h_map.keys():
-                if stack == [] or h_map[char] != stack.pop():
+                #stack == [] stack is empty, closed로만 이뤄져있음
+                #
+                if stack == [] or h_map[char] != stack.pop():   
                     return False
             else:
                 return False
-        return stack == []
+            
+        return stack == []  #return true when stack is empty
