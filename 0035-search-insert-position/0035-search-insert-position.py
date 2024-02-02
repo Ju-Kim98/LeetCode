129eed4f9c80ae2d35531d866763a9411b1a9370
Ju-Kim98 <130087422+Ju-Kim98@users.noreply.github.com>
2024-02-01 17:20:54 -0800

    Time: 28 ms (71.31%), Space: 12.5 MB (78.85%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,17 +1,31 @@
-class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        left = 0
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+       
+    # O(logn) time
+        
+        left = 0;
         right = len(nums)-1
         
         while left<=right:
             mid = (left+right)//2
             
-            if nums[mid]==target:
+            if nums[mid] == target:
                 return mid
             elif nums[mid]<target:
                 left=mid+1
+                #return left
             else:
-                right=mid-1
-                
+                right = mid-1
+                #return right
         return left
+            
+            #target이 없는 경우 
+            
+        
+        
         
