75fcea77b4c4de46ba8ec2ff2d3b724e31a79562
Ju-Kim98 <130087422+Ju-Kim98@users.noreply.github.com>
2024-02-01 20:21:11 -0800

    Time: 97 ms (63.51%), Space: 12.8 MB (98.72%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -14,6 +14,7 @@ class Solution(object):
         
         while left < right:  
             mid = (left + right) // 2
+            
             if nums[mid] > mid: 
                 right = mid
             else:  
