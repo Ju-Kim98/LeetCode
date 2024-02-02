5df8f1de1c15a45c48fc447cfbc428d81a9afc6c
Ju-Kim98 <130087422+Ju-Kim98@users.noreply.github.com>
2024-02-01 16:12:33 -0800

    Time: 102 ms (99.11%), Space: 12.9 MB (93.18%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,12 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        
+       
+    #O(n) time
+    
         zero_move = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                if i>zero_move:
-                    nums[zero_move], nums[i] = nums[i], nums[zero_move]
+                nums[zero_move], nums[i] = nums[i], nums[zero_move]     #두 배열의 요소 값 swap
                 zero_move += 1
                 
+    #[0,1,0,3,12]
+    #i = 0, nums[0]=1 != 0 -> 0, 1 = 1, 0 
                 
