77a951369b0803d1db0ff38fe9f08af7e9f88519
Ju-Kim98 <130087422+Ju-Kim98@users.noreply.github.com>
2024-02-01 16:15:23 -0800

    Time: 117 ms (66.54%), Space: 12.9 MB (93.18%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -15,10 +15,6 @@ class Solution(object):
                 
    
 
-
-
 #memo
-
-    # 오른쪽에 있는 nums[i], nums[lastNonZeroFoundAt]를 평가합니다. 이는 nums[i]와 nums[lastNonZeroFoundAt]의 값이 포함된 튜플을 생성합니다. 이 단계에서 이 두 요소의 현재 값이 임시로 저장됩니다, 이는 교환을 가능하게 합니다.
-
-# nums[lastNonZeroFoundAt]에 할당하고, 튜플의 두 번째 요소를 nums[i]에 할당합니다. 이 할당은 동시에 발생합니다, 즉 nums[lastNonZeroFoundAt]와 nums[i]는 동시에 새로운 값으로 업데이트됩니다.
+# 오른쪽 nums[i], nums[zero_move] => nums[i]와 nums[zero_move]의 값이 포함된 튜플을 생성 후 두 요소의 현재 값이 임시로 저장-
+# 왼쪽 nums[zero_move]과 튜플의 두 번째 요소를 nums[i]에 동시에 할당
