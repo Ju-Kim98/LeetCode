b1c52ad244885e3fe6090e1a37adf62fcdb34451
Ju-Kim98 <130087422+Ju-Kim98@users.noreply.github.com>
2024-02-01 20:20:10 -0800

    Time: 104 ms (44.01%), Space: 13.1 MB (82.14%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+                
+        # Use sort, binary search
+        
+        nums.sort()
+        
+        left = 0;
+        right = len(nums)
+        
+        while left < right:  
+            mid = (left + right) // 2
+            if nums[mid] > mid: 
+                right = mid
+            else:  
+                left = mid + 1
+        
+        return left
+                
