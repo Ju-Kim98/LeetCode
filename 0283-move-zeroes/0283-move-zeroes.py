e3418c818d12984f2565c37b362143c4bc79e240
Ju-Kim98 <130087422+Ju-Kim98@users.noreply.github.com>
2024-02-01 16:13:40 -0800

    Time: 123 ms (46.31%), Space: 12.7 MB (99.71%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -13,6 +13,12 @@ class Solution(object):
                 nums[zero_move], nums[i] = nums[i], nums[zero_move]     #두 배열의 요소 값 swap
                 zero_move += 1
                 
-    #[0,1,0,3,12]
-    #i = 0, nums[0]=1 != 0 -> 0, 1 = 1, 0 
-                
+   
+
+
+
+#memo
+
+    # 오른쪽에 있는 nums[i], nums[lastNonZeroFoundAt]를 평가합니다. 이는 nums[i]와 nums[lastNonZeroFoundAt]의 값이 포함된 튜플을 생성합니다. 이 단계에서 이 두 요소의 현재 값이 임시로 저장됩니다, 이는 교환을 가능하게 합니다.
+
+# nums[lastNonZeroFoundAt]에 할당하고, 튜플의 두 번째 요소를 nums[i]에 할당합니다. 이 할당은 동시에 발생합니다, 즉 nums[lastNonZeroFoundAt]와 nums[i]는 동시에 새로운 값으로 업데이트됩니다.
