1be096c169e11d14d96c461987f95adebba344ad
Ju-Kim98 <130087422+Ju-Kim98@users.noreply.github.com>
2024-02-08 16:15:11 -0800

    Time: 797 ms (36.39%), Space: 20.9 MB (80.03%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,17 +4,31 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-    
-        min_price = float('inf')     #initialize some large value using float('inf')
-        profit = 0
+        #binary search
         
-        for price in prices:
-            if price < min_price:
-                min_price = price
-            elif price - min_price > profit:
-                profit = price - min_price
-                
-        return profit
-                
+        left = 0    #Buy price
+        right = 1   #Sell price
+        max_profit = 0
+        while right < len(prices):
+            currentProfit = prices[right] - prices[left]    #current Profit
+            if prices[left] < prices[right]:
+                max_profit =max(currentProfit,max_profit)
+            else:
+                left = right
+            right += 1
+        return max_profit
         
-        #Array, Dyanamic Programming
+         #Array, Dyanamic Programming   
+#         min_price = float('inf')     #initialize some large value using float('inf')
+#         profit = 0
+        
+#         for price in prices:
+#             if price < min_price:
+#                 min_price = price
+#             elif price - min_price > profit:
+#                 profit = price - min_price
+                
+#         return profit
+                
+
+      
