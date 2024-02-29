05040bdea345ecdcab26adffc3324511fc11ea89
Ju-Kim98 <130087422+Ju-Kim98@users.noreply.github.com>
2024-02-29 14:47:05 -0800

    Time: 15 ms (54.41%), Space: 11.7 MB (66.91%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.py
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.py
@@ -5,14 +5,15 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        num_str = str(num)  # Convert number to string
-        count = 0  # Initialize counter for k-beauty
+        num_str = str(num) #convert num to string (정수를 문자열로  )
+        count = 0  
+        l = len(num_str)
         
-        for i in range(len(num_str) - k + 1):
-            substring = num_str[i:i+k]  # Extract substring of length k
-            divisor = int(substring)  # Convert substring back to integer
+        for i in range(l - k + 1):
+            substring = num_str[i:i+k]  # 길이 k인 (index i부터 i+k) substring 
+            divisor = int(substring)  #substring 배열을 정수로
             
-            if divisor != 0 and num % divisor == 0:  # Check if it's a valid divisor
-                count += 1  # Increment counter if conditions are met
+            if divisor != 0 and num % divisor == 0: 
+                count += 1 
                 
         return count
