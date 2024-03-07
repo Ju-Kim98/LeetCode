c25c7afcc73ef77eba33805113b44f331771fb19
Ju-Kim98 <130087422+Ju-Kim98@users.noreply.github.com>
2024-03-07 14:37:26 -0800

    Time: 22 ms (23.80%), Space: 11.8 MB (95.19%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def reverseStr(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        
+        #two pointers 
+        
+        new_list = list(s)  #change string to list 
+        
+        for start in range(0, len(s), 2 * k):
+            left = start
+            right = min(start + k - 1, len(s) - 1)
+            
+            while left < right:
+                new_list[left], new_list[right] = new_list[right], new_list[left]
+                left += 1
+                right -= 1
+        
+        return ''.join(new_list)      # Join the list back into a string
+
+        
+        #String: solution: O(n) time, O(n) space
+        # l = list(s)   
+        # for i in range(0,len(l),2*k):
+        #     l[i:i+k] = reversed(l[i:i+k])   
+        # return "".join(l)
+    
