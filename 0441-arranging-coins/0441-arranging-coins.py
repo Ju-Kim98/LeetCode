8abfa39c6524066d2eb7585a71612579815f2959
Ju-Kim98 <130087422+Ju-Kim98@users.noreply.github.com>
2024-03-07 14:52:10 -0800

    Time: 15 ms (88.87%), Space: 11.7 MB (54.01%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def arrangeCoins(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        bottom = 1
+        top = n
+        
+        while bottom <= top :
+            mid = bottom + (top - bottom) // 2
+            coins = mid* (mid + 1) / 2
+            if coins == n:
+                return mid
+            if coins < n:
+                bottom = mid + 1
+            else:
+                top = mid - 1
+        return top
