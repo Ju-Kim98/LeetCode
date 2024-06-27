75dce25dee2283e370291d7e6c2f1cec3bcadfc7
Ju <130087422+Ju-Kim98@users.noreply.github.com>
2024-06-27 11:00:26 -0700

    1757-recyclable-and-low-fat-products.sql

--- 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -6,4 +6,12 @@ FROM
     Products
 WHERE
     low_fats = 'Y' AND recyclable = 'Y'
+
+
+-- import pandas as pd
+
+-- def find_products(products: pd.DataFrame) -> pd.DataFrame:
+--     df = products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')]
+--     df = df[['product_id']]
     
+--     return df
