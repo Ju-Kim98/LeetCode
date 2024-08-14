52c0d00b73489280285b09f0715308deec4128a5
Ju <130087422+Ju-Kim98@users.noreply.github.com>
2024-08-14 15:18:00 -0700

    Time: 32 ms, Memory: 11.8 MB - LeetHub

--- unique-email-addresses/unique-email-addresses.py
+++ unique-email-addresses/unique-email-addresses.py
@@ -9,10 +9,20 @@ class Solution(object):
         elif len(emails) == 1:
             return 1
         else: 
-            email_set = set()
-            for mail in emails: 
-                mail = mail.split('@')
-                local = ''.join(mail[0].replace('.','').split('+')[0])
-                domain = mail[-1]
+            email_set = set()       # set = {"name@leetcode.com", "email.name@email.com", ... }
+            
+            for mail in emails:     # mail은 email list 요소들
+                mail = mail.split('@')  #이메일을 @를 기준으로 , then mail[0](left part of list) is local part and mail[-1](right part of list) is domain part
+                
+                # 뒤에서부터 읽어야함
+                # mail[0]부분의 요소를 + 로 split 하고 그 오른쪽 부분에서 .와  ''를 제거하고 
+                # ''.join() -> join the processed string back together, but in this case, it's unnecessary
+                #local = ''.join(mail[0].replace('.','').split('+')[0])  
+                local = mail[0].replace('.','').split('+')[0]
+                
+                domain = mail[-1]       # list[-1] index is regfers the last element of the list
                 email_set.add(local+'@'+domain)
         return len(email_set)
+    
+    
+    # join() function is used when a list of strings that you want to combine into a single string. 
