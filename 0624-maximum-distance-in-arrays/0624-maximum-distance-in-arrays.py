57a8174f9c438d12821a7dda57d7e05a203f151c
Ju <130087422+Ju-Kim98@users.noreply.github.com>
2024-08-15 23:32:40 -0700

    Time: 1355 ms (46.15%), Space: 42.4 MB (43.59%) - LeetHub

---
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxDistance(self, arrays):
+        """
+        :type arrays: List[List[int]]
+        :rtype: int
+        """
+        cur_min, cur_max = arrays[0][0], arrays[0][-1]
+        res = 0 
+        
+        for i in range(1, len(arrays)):
+            arr = arrays[i]
+            res = max(res, arr[-1]-cur_min, cur_max-arr[0])
+            cur_min = min(cur_min, arr[0])
+            cur_max = max(cur_max, arr[-1])
+            
+        return res
