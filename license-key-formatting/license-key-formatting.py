6ccc137dbdbf7b963ae43c6d311289da22d94296
Ju <130087422+Ju-Kim98@users.noreply.github.com>
2024-08-16 13:57:12 -0700

    Time: 66 ms, Memory: 14.1 MB - LeetHub

---
+++ license-key-formatting/license-key-formatting.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def licenseKeyFormatting(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        n = len(s)
+        count = 0 
+        ans = ['']
+        
+        for i in reversed(range(n)):
+            if(s[i] != '-'):
+                ans += s[i].upper()
+                count = count + 1
+                if (count == k):
+                    count = 0
+                    ans += '-'
+                    
+        # output doesn't start w/ dash
+        if (len(ans)>0 and ans[len(ans)-1] == '-'):
+            ans = ans[:-1]
+        ans = ans[::-1]
+        result = "".join(ans)
+        return result
