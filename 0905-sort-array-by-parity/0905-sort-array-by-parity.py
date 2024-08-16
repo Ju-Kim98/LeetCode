31b3b6870d1cfe15e56daed0ae56cc1a89ef8284
Ju <130087422+Ju-Kim98@users.noreply.github.com>
2024-08-16 13:37:45 -0700

    Time: 48 ms (73.91%), Space: 12.5 MB (67.01%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.py
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -1,14 +1,8 @@
-class Solution:
-    def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        
-        evenL = []
-        oddL = []
-        
-        for i in range(len(nums)):
-            if nums[i]%2 == 0:  #짝홀 구분
-                evenL.append(nums[i])
-            else:
-                oddL.append(nums[i])
-        
-        return evenL + oddL
-        
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.sort(key = lambda x : x%2)
+        return nums
