bcf919b4e8e5cf8a4610f94c65c631267cccd9cc
Ju <130087422+Ju-Kim98@users.noreply.github.com>
2024-08-21 04:54:32 -0700

    Time: 99 ms (98.78%), Space: 21.9 MB (98.78%) - LeetHub

---
+++ 0247-strobogrammatic-number-ii/0247-strobogrammatic-number-ii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findStrobogrammatic(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        strobo_nums = {'1':'1','6':'9','8':'8','9':'6'}
+        def recurse(n, isFinal):
+            if n == 0:
+                return ['']
+            if n == 1:
+                return ['0', '1', '8']
+            prev = recurse(n-2, False)
+            res = [d+c+strobo_nums[d] for c in prev for d in strobo_nums]
+            if not isFinal:
+                res.extend(['0'+c+'0' for c in prev])
+            return res
+        return recurse(n, True)
