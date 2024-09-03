6157e0fd9ed2d43b7831f4199e92a8bfe66f10c2
Ju <130087422+Ju-Kim98@users.noreply.github.com>
2024-09-03 15:22:32 -0700

    Time: 16 ms, Memory: 11.6 MB - LeetHub

--- rotate-image/rotate-image.py
+++ rotate-image/rotate-image.py
@@ -5,31 +5,23 @@ class Solution(object):
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
 
-        l, r = 0, len(matrix) - 1
+        self.transpose(matrix)
+        self.reflect(matrix)
 
-        while l < r:
-            for i in range(r - l):
-                top, bottom = l, r
+    def transpose(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
 
-                # save the top left
-                topLeft = matrix[top][l + i]
+    def reflect(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][-j - 1] = (
+                    matrix[i][-j - 1],
+                    matrix[i][j],
+                )
 
-                # move bottom left into top left
-                matrix[top][l + i] = matrix[bottom - i][l]
-
-                # move bottom right into bottom left
-                matrix[bottom - i][l] = matrix[bottom][r - i]
-
-                # move top right into bottom right
-                matrix[bottom][r - i] = matrix[top + i][r]
-
-                # move top left into top right
-                matrix[top + i][r] = topLeft
-
-            r -= 1
-            l += 1
-
-                
-                
                 
                 
