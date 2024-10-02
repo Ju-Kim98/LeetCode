9637060409d1037a57e851a89790bd29fa14cbbb
Ju <130087422+Ju-Kim98@users.noreply.github.com>
2024-10-02 14:42:17 -0700

    Time: 7 ms (97.41%), Space: 11.6 MB (82.32%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,20 +1,16 @@
-class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
-        nx = [{} for _ in range(len(t)+1)]
-        for i in range(len(t)-1, -1, -1):
-            nx[i] = nx[i + 1].copy()
-            nx[i][t[i]]= i+1
-            
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
         i = 0
-        for c in s:
-            if c in nx[i]:
-                i = nx[i][c]
-            else:
-                return False
+        j = 0
         
-        return True
-    
-    
-    # Dynamic Programming
-    
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
             
+        return i == len(s)
