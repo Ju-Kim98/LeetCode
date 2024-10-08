395180aa468aabb08a90f77003e0d1c7cbc519ab
Ju <130087422+Ju-Kim98@users.noreply.github.com>
2024-10-07 22:36:57 -0700

    Time: 36 ms (46.06%), Space: 11.9 MB (94.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,31 +5,13 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        # case1: Sort 두 array를 알파벳순으로 sort하고 서로 요소가 같은지 확인하기
-        return sorted(s) == sorted(t)  #O(nlogn)
-
-    
-    """  
-    #case1: with two count array
-        return Counter(s) == Counter(t)
-    
+        
         if len(s) != len(t):
             return False 
         
-        countS, countT = {}, {}
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i],0)
-            countT[t[i]] = 1 + countS.get(t[i],0)
-        for c in countS:
-            if countS[c] != countT.get(c,0):
-                return False
-            
-        return True
-"""
+        return Counter(s) == Counter(t)
+
         
     
     
-    
-    # case1) 
-    # case2)
     
