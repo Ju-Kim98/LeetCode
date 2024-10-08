a0b7f4222190f016f58675474ab6a64a6c9ffe90
Ju <130087422+Ju-Kim98@users.noreply.github.com>
2024-10-07 22:37:06 -0700

    Time: 38 ms (37.40%), Space: 11.8 MB (94.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -6,8 +6,8 @@ class Solution(object):
         :rtype: bool
         """
         
-        if len(s) != len(t):
-            return False 
+#         if len(s) != len(t):
+#             return False 
         
         return Counter(s) == Counter(t)
 
