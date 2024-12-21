52f82ec28cf6a7655e6ff5f56692df7aa1f51296
Ju <130087422+Ju-Kim98@users.noreply.github.com>
2024-12-21 15:49:47 -0800

    Time: 4 ms (78.85%), Space: 12.6 MB (6.67%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def partitionLabels(self, s):
+        """
+        :type s: str
+        :rtype: List[int]
+        """
+        
+        last = {c: i for i, c in enumerate(s)}
+        j = anchor = 0
+        ans = []
+        for i, c in enumerate(s):
+            j = max(j, last[c])
+            if i == j:
+                ans.append(i-anchor+1)
+                anchor = i + 1
+                
+        return ans
