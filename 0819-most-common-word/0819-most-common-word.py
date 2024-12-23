8edb26a506b15efb5157ec009cc940d5cce1771e
Ju <130087422+Ju-Kim98@users.noreply.github.com>
2024-12-23 01:32:03 -0800

    Time: 3 ms (79.32%), Space: 12.6 MB (8.35%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def mostCommonWord(self, paragraph, banned):
+        """
+        :type paragraph: str
+        :type banned: List[str]
+        :rtype: str
+        """
+        
+        normalized = ''.join([c.lower() if c.isalnum() else ' ' for c in paragraph])
+        
+        words = normalized.split()
+        
+        word_cnt = defaultdict(int)
+        banned_word = set(banned)
+        
+        for w in words:
+            if w not in banned_word:
+                word_cnt[w] += 1
+                
+        res= max(word_cnt.items(), key=operator.itemgetter(1))[0]
+        
+        return res
+        
+        #return max(word_cnt.items(), key=operator.itemgetter(1))[0]
+    #most_common_word = max(word_cnt.items(), key=lambda item: item[1])[0]
+
+        
