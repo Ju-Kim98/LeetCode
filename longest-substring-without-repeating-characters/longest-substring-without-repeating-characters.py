f89c5d5c6153b53f7074d3bafb5a41e9e6d8a007
Ju <130087422+Ju-Kim98@users.noreply.github.com>
2024-12-23 15:16:25 -0800

    Time: 19 ms, Memory: 13.4 MB - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -5,18 +5,13 @@ class Solution(object):
         :rtype: int
         """
         # sliding window
-        n = len(s)
         res = 0
         cur_char = {}
         i = 0
         
-        for j in range(n):
+        for j in range(len(s)):
             if s[j] in cur_char:
                 i = max(cur_char[s[j]], i)
             res = max(res, j-i+1)
             cur_char[s[j]] = j+1
-            
         return res
-        
-        
-        
