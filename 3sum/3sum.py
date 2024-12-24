5f1110915ebaaeab7b10cdc12312d30ed627ee04
Ju <130087422+Ju-Kim98@users.noreply.github.com>
2024-12-23 16:58:24 -0800

    Time: 546 ms, Memory: 18.6 MB - LeetHub

--- 3sum/3sum.py
+++ 3sum/3sum.py
@@ -4,31 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        # two pointer, expand of twosum
-        
         res = []
         nums.sort()
         for i in range(len(nums)):
             if nums[i] > 0:
                 break
-            if i == 0 or nums[i - 1] != nums[i]:
+            if i == 0 or nums[i-1] != nums[i]:
                 self.twoSum(nums, i, res)
         return res
-        
-        
+    
     def twoSum(self, nums, i, res):
-        lo, hi = i + 1, len(nums) - 1
-        while lo < hi:
-            sum = nums[i] + nums[lo] + nums[hi]
-            if sum < 0:
+        lo, hi = i+1, len(nums)-1
+        while lo<hi:
+            sum = nums[i]+nums[lo]+nums[hi]
+            if sum<0:
                 lo += 1
-            elif sum > 0:
+            elif sum>0:
                 hi -= 1
             else:
                 res.append([nums[i], nums[lo], nums[hi]])
                 lo += 1
                 hi -= 1
-                while lo < hi and nums[lo] == nums[lo - 1]:
+                while lo<hi and nums[lo] == nums[lo-1]:
                     lo += 1
-        
-        
