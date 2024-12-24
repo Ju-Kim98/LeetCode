c30c4c65667d8a94a0eff28594868fb94d096167
Ju <130087422+Ju-Kim98@users.noreply.github.com>
2024-12-23 16:46:50 -0800

    Time: 532 ms (94.82%), Space: 18.6 MB (18.29%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -4,22 +4,31 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        # two pointer, expand of twosum
+        
         res = []
         nums.sort()
-        
-        for i, a in enumerate(nums):
-            if i > 0 and a == nums[i-1]:
-                continue
-            left, right = i+1, len(nums)-1
-            while left < right:
-                numSum = a + nums[left] + nums[right]
-                if numSum > 0:
-                    right -= 1
-                elif numSum < 0:
-                    left += 1
-                else:
-                    res.append([a, nums[left], nums[right]])
-                    left += 1
-                    while nums[left] == nums[left-1] and left < right:
-                        left += 1
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            if i == 0 or nums[i - 1] != nums[i]:
+                self.twoSum(nums, i, res)
         return res
+        
+        
+    def twoSum(self, nums, i, res):
+        lo, hi = i + 1, len(nums) - 1
+        while lo < hi:
+            sum = nums[i] + nums[lo] + nums[hi]
+            if sum < 0:
+                lo += 1
+            elif sum > 0:
+                hi -= 1
+            else:
+                res.append([nums[i], nums[lo], nums[hi]])
+                lo += 1
+                hi -= 1
+                while lo < hi and nums[lo] == nums[lo - 1]:
+                    lo += 1
+        
+        
