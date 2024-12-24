7aab71177070e0280594037aef9367eb66759d3b
Ju <130087422+Ju-Kim98@users.noreply.github.com>
2024-12-23 16:09:38 -0800

    Time: 18 ms (16.37%), Space: 12.4 MB (8.77%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -4,6 +4,34 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
+        
+        digits = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ]
+        
+        RD = [] #roman digits
+        for val, sym in digits:
+            if num == 0:
+                break
+            count, num = divmod(num, val)   #divmod: (8 // 2, 8 % 2) 몫,나머지
+            RD.append(sym*count)
+            
+        return "".join(RD)
+        
+        
+        """
         thousand = ["", "M", "MM", "MMM"]
         hundred = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
         ten = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
@@ -15,3 +43,4 @@ class Solution(object):
         )
         
         return res
+        """
