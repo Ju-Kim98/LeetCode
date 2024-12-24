77fa62447f5ecbe9a720691c06629d8ddd589c6a
Ju <130087422+Ju-Kim98@users.noreply.github.com>
2024-12-23 16:18:17 -0800

    Time: 6 ms, Memory: 12.5 MB - LeetHub

--- integer-to-roman/integer-to-roman.py
+++ integer-to-roman/integer-to-roman.py
@@ -4,14 +4,27 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
-        thousand = ["", "M", "MM", "MMM"]
-        hundred = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
-        ten = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
-        one = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        digits = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ]
         
-        res = (
-            thousand[num//1000]+hundred[num%1000//100]
-            +ten[num%100//10]+one[num%10]
-        )
-        
-        return res
+        RD = []
+        for val, sym in digits:
+            if num == 0:
+                break
+            count, num = divmod(num, val)  # count = num//val, num = num%val
+            RD.append(sym*count)
+            
+        return "".join(RD)
